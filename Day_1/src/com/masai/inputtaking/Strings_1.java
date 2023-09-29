7142d714141d3e39957967451a1b744380778833
Ramesh Yadav <113420314+rameshy9891@users.noreply.github.com>
2023-09-29 08:44:51 +0530

    five

---
+++ Day_1/src/com/masai/inputtaking/Strings_1.java
@@ -0,0 +1,32 @@
+package com.masai.inputtaking;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Strings_1 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in);
+		
+		
+		System.out.println( "plse enter the length of string");
+		
+		int len= sc.nextInt();
+		
+		sc.nextLine();
+		System.out.println("plse enter the string ");
+		String str= sc.nextLine();
+		
+	  char[] charArr= str.toCharArray();
+	  
+	  //System.out.println(Arrays.toString(charArr));
+	  
+	  Arrays.sort(charArr);
+	  
+	  for(char ch : charArr) {
+		  System.out.print(ch+" ");
+	  }
+	}
+
+}
