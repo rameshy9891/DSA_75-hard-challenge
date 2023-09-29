b27af2c8037a30c770982e2b452b155e71eabb36
Ramesh Yadav <113420314+rameshy9891@users.noreply.github.com>
2023-09-29 07:15:13 +0530

    first

---
+++ Day_1/src/com/masai/inputtaking/SingleLineSingleInteger.java
@@ -0,0 +1,18 @@
+package com.masai.inputtaking;
+
+import java.util.Scanner;
+
+public class SingleLineSingleInteger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.println("plse enter the  value");
+		
+		System.out.println(sc.nextInt()+1);
+
+	}
+
+}
