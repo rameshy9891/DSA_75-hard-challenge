5193ce50735ef749471379b488ce5a88888558db
Ramesh Yadav <113420314+rameshy9891@users.noreply.github.com>
2023-09-30 07:07:13 +0530

    modof10

---
+++ Day_2/src/com/masai/easyQuestion/ModOf10.java
@@ -0,0 +1,34 @@
+package com.masai.easyQuestion;
+
+import java.util.Scanner;
+
+public class ModOf10 {
+	/*
+	 * we have find mod of any number
+	 * 
+	 * sample input : 7
+	 * sample output : 1 2 3 4 5 6 7 
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("plse enter the which value you find of mod");
+		int tar= sc.nextInt();
+		
+		for(int i=1; i<=tar; i++) {
+			int res= i%10;
+			
+			if(res==0) {
+				break;
+			}
+			
+			System.out.print(res+" ");
+		}
+		
+		
+
+	}
+
+}
