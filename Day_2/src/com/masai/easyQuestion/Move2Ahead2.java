3dd92b0567591d4544d5e5ef1d07a9ef79707ae4
Ramesh Yadav <113420314+rameshy9891@users.noreply.github.com>
2023-09-30 06:28:47 +0530

    move2Ahead2  (two)

---
+++ Day_2/src/com/masai/easyQuestion/Move2Ahead2.java
@@ -0,0 +1,23 @@
+package com.masai.easyQuestion;
+
+import java.util.Scanner;
+
+public class Move2Ahead2 {
+	
+	// we have to add 2 all value and start with 2 till to the target
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc= new Scanner(System.in);
+		
+		int tar= sc.nextInt();
+		
+		for(int i=2; i<=tar; i++) {
+			System.out.println(i);
+			i++;
+		}
+
+	}
+
+}
